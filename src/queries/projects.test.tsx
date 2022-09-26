27a9f95a36091dc451c636e512c644a03d3d1194
pykulytsky <pykulytsky@gmail.com>
2022-09-26 22:33:06 +0300

    test for projects query

--- src/queries/projects.test.tsx
+++ src/queries/projects.test.tsx
@@ -1,33 +1,65 @@
 import { describe, test, expect } from "vitest"
 import { renderHook, waitFor } from "@testing-library/react"
-import { useProject } from "./projects"
+import { useOwnProjects, useProject } from "./projects"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import MockAdapter from "axios-mock-adapter"
 import http from "../api/axios"
 
-describe("custom project query hook", () => {
-  const queryClient = new QueryClient()
-  const wrapper = ({ children }) => (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  )
-  let mock = new MockAdapter(http)
-  mock.onGet("/projects/1").reply(200, {
+let mock = new MockAdapter(http)
+
+const queryClient = new QueryClient()
+// @ts-ignore
+const wrapper = ({ children }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+)
+
+describe("own projects", () => {
+  const mockData = [
+    {
+      name: "Development",
+      description: "string",
+    },
+    {
+      name: "test project",
+      description: "string",
+    },
+    {
+      name: "test",
+      description: "string",
+    },
+  ]
+  mock.onGet("/projects/user/").reply(200, mockData)
+  const { result } = renderHook(() => useOwnProjects(), { wrapper })
+  test("own projects query should be successfull", async () => {
+    await waitFor(() => expect(result.current.isSuccess).toBe(true))
+    await waitFor(() => expect(result.current.isError).toBe(false))
+  })
+  test("query resulted data", async () => {
+    await waitFor(() => expect(result.current.data).toHaveLength(3))
+  })
+  test("query resulted data should contain correct amount of projects", async () => {
+    await waitFor(() => expect(result.current.data).toHaveLength(3))
+  })
+})
+
+describe("exact project", () => {
+  const mockData = {
     name: "Development",
     description: "string",
-    icon: "🌈",
-    accent_color: "blue",
-    is_favorite: true,
-    is_pinned: true,
-    is_editable: true,
-    show_completed_tasks: false,
-    id: 1,
-    owner_id: 156,
-    tasks: [],
-    sections: [],
-  })
-  test("should be successfull", async () => {
-    const { result } = renderHook(() => useProject(1), { wrapper })
-
+  }
+  mock.onGet("/projects/1").reply(200, mockData)
+  const { result } = renderHook(() => useProject(1), { wrapper })
+  test("fetching exact project should be successfull", async () => {
     await waitFor(() => expect(result.current.isSuccess).toBe(true))
+    await waitFor(() => expect(result.current.isError).toBe(false))
+  })
+  test("query resulted data", async () => {
+    await waitFor(() => expect(result.current.data).toEqual(mockData))
+  })
+  test("query data is fetched", async () => {
+    await waitFor(() => expect(result.current.isFetched).toBe(true))
+  })
+  test("query data should be an object", async () => {
+    await waitFor(() => expect(result.current.data).toBeTypeOf("object"))
   })
 })
