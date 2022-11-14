c76330556a599aa716e89f83cd34ba7179ac2068
pykulytsky <pykulytsky@gmail.com>
2022-11-14 17:41:27 +0200

    implement sider resizing

--- src/layout/Aside/Aside.styles.ts
+++ src/layout/Aside/Aside.styles.ts
@@ -1,6 +1,9 @@
 import { createStyles } from "@mantine/core"
 
 export const useStyles = createStyles((theme) => ({
+  wrapper: {
+    display: "flex",
+  },
   root: {
     backgroundColor:
       theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.white,
@@ -15,5 +18,21 @@ export const useStyles = createStyles((theme) => ({
       theme.colorScheme === "light"
         ? "rgba(100, 100, 111, 0.2) -4px 5px 12px 0px"
         : "rgba(14, 14, 14, 0.2) -4px 5px 12px 0px",
+  },
+  resizeHandle: {
+    padding: 0,
+    marginTop: 16,
+    height: "calc(100vh - 46px)",
+    width: 3,
+    backgroundColor: "red",
+    transform: "translateX(5px)",
+    zIndex: 9999,
+    opacity: 0,
+    transition: "250ms opacity ease-out",
+    cursor: "col-resize",
+
+    "&:hover": {
+      opacity: 1,
+    },
   },
 }))
--- src/layout/Aside/Aside.tsx
+++ src/layout/Aside/Aside.tsx
@@ -1,6 +1,6 @@
 import { Aside as AsideComponent, MediaQuery } from "@mantine/core"
 import { AnimatePresence, motion } from "framer-motion"
-import { ReactNode } from "react"
+import { ReactNode, useState, useRef, useCallback, useEffect } from "react"
 import AsideBottom from "../../components/aside/AsideBottom/AsideBottom"
 import AsideContent from "../../components/aside/AsideContent/AsideContent"
 import AsideHeader from "../../components/aside/AsideHeader/AsideHeader"
@@ -16,18 +16,63 @@ export type AsideProps = {
 
 export default function Aside(props: AsideProps) {
   const { classes } = useStyles()
+
+  const sidebarRef = useRef(null)
+  const [isResizing, setIsResizing] = useState(false)
+  const [prefferedWidth, setWidth] = useState(400)
+
+  const startResizing = useCallback(() => {
+    setIsResizing(true)
+  }, [])
+
+  const stopResizing = useCallback(() => {
+    setIsResizing(false)
+  }, [])
+
+  const resize = useCallback(
+    (mouseMoveEvent: any) => {
+      if (isResizing) {
+        setWidth(
+          //@ts-ignore
+          sidebarRef?.current?.getBoundingClientRect().right -
+            mouseMoveEvent.clientX
+        )
+        console.log(prefferedWidth)
+      }
+    },
+    [isResizing]
+  )
+
+  useEffect(() => {
+    window.addEventListener("mousemove", resize)
+    window.addEventListener("mouseup", stopResizing)
+    return () => {
+      window.removeEventListener("mousemove", resize)
+      window.removeEventListener("mouseup", stopResizing)
+    }
+  }, [resize, stopResizing])
+
   return (
-    <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
+    <MediaQuery
+      //@ts-ignore
+      onMouseDown={(e) => e.preventDefault()}
+      smallerThan="sm"
+      styles={{ display: "none" }}
+    >
       <AnimatePresence>
         {props.opened && (
           <motion.div
+            ref={sidebarRef}
+            className={classes.wrapper}
+            onMouseDown={(e) => e.preventDefault()}
             initial={{ width: 0 }}
             animate={{
-              width: props.opened ? "28vw" : 0,
+              width: props.opened ? prefferedWidth : 0,
               margin: props.opened ? 8 : 0,
             }}
             exit={{ width: 0 }}
           >
+            <div className={classes.resizeHandle} onMouseDown={startResizing} />
             <AsideComponent className={classes.root}>
               <AsideHeader toggleAside={props.toggle} actions={props.actions} />
               <AsideContent>{props.content}</AsideContent>
